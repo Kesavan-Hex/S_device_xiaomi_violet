687f5dd7cde2d1d7b541d27ad9fffa3415dbb354
KesavanG <kesavang4020@gmail.com>
2021-11-11 19:56:52 +0530

    violet:add kernel configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,3 +34,26 @@ TARGET_2ND_CPU_VARIANT := generic
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE :=  4096
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
+BOARD_RAMDISK_OFFSET := 0x01000000
+
+TARGET_KERNEL_CONFIG := vendor/violet-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
