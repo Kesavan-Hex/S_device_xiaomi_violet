a137e11fa2b665512b78451711ab5fda0355a06e
KesavanG <kesavang4020@gmail.com>
2021-11-12 20:14:58 +0530

    violet:config screen density and boot animation res

--- device.mk
+++ device.mk
@@ -23,7 +23,17 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/violet/violet-vendor.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Screen Density
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+
 
