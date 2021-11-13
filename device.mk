db3d1d000584e2aadf49ca32d32ccd5eff5c71b0
KesavanG <kesavang4020@gmail.com>
2021-11-13 21:47:51 +0530

    violet: Import wifi configs from stock Reference https://dumps.tadiphone.dev/dumps/xiaomi/violet/-/tree/violet-user-10-QKQ1.190915.002-V12.5.1.0.QFHCNXM-release-keys/vendor/etc/wifi

--- device.mk
+++ device.mk
@@ -171,6 +171,12 @@ PRODUCT_TARGET_VNDK_VERSION := 29
 PRODUCT_PACKAGES += \
     vndk_package
 
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
